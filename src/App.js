a2acbb1ac47818efa8007741c6c5519495e04b88
keith1101 <anhhuybui2006@gmail.com>
2026-02-27 15:11:11 +0700

    feat: Implement initial frontend structure with Home and Generator pages, including routing and styling.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import CreatePage from './pages/CreatePage'; // added CreatePage
+import GeneratorPage from './pages/GeneratorPage';
 import './App.css';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/create" element={<CreatePage />} />
+        <Route path="/generator" element={<GeneratorPage />} />
       </Routes>
     </Router>
   );
