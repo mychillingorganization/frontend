76f5aa4e93ecaf4cd332a8f11c875c1cf4846334
keith1101 <anhhuybui2006@gmail.com>
2026-02-27 08:36:03 +0700

    feat: update logo, "Create" page creation

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import CreatePage from './pages/CreatePage'; // added CreatePage
 import './App.css';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/create" element={<CreatePage />} />
       </Routes>
     </Router>
   );
